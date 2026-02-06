d5deec3ef9890482a948e66e6007232f87a7f4c2
unknown <javierballestervillalba@gmail.com>
2026-02-06 20:22:41 +0100

    Lambda ZIP added to git ignore and logger.py modified to not duplicate loggings.

--- app/logger.py
+++ app/logger.py
@@ -4,17 +4,16 @@ from app.config import settings
 def get_logger(name: str) -> logging.Logger:
 
     logger = logging.getLogger(name)
-
-    if logger.handlers:
-
-        return logger
     
     logger.setLevel(settings.LOG_LEVEL)
 
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+    logger.propagate = False
 
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
     return logger
